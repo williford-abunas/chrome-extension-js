a56ce94e29f54a94686b3a1a19d00d7342976bf5
williford abunas <abunaswilliford.com>
2024-01-25 22:00:27 +1300

    event listener for save btn

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const inputEl = document.getElementById('input-el')
+const saveBtn = document.getElementById('save-btn')
+const ulEl = document.getElementById('ul-el')
+let myLeads = []
+let items = ''
 
+saveBtn.addEventListener('click', function () {
+  myLeads.push(inputEl.value)
+  render(myLeads)
+  items = ''
+  inputEl.value = ''
+})
+
+function render(leads) {
+  for (let i = 0; i < leads.length; i++) {
+    items += `<li><a href="${leads[i]}">${leads[i]}</a></li>`
+  }
+  ulEl.innerHTML = items
+}
